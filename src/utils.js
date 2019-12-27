4783fcc795936ed378f511ed73d759279b4906d4
Roman Stankiewicz <roman.stan3@gmail.com>
2019-12-27 00:53:29 +0000

    space

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,6 @@
 import React from "react"
 import {useStateContext} from "./store"
 
-
 export const connect = (props, actions) => (Component) => (rest) => {
   const {state, dispatch} = useStateContext()
 

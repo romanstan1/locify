c9f49d5d7625212242cdb1ed9fc4819dd38968a4
Roman Stankiewicz <roman.stan3@gmail.com>
2020-01-30 09:59:23 +0000

    Changes SignUp

---
+++ src/pages/SignUp.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {PageHeading} from "../components/Layout"
+
+const SignUp = () => {
+  return (
+    <>
+      <PageHeading>
+        Sign up
+      </PageHeading>
+    </>
+  )
+}
+
+
+export default SignUp
